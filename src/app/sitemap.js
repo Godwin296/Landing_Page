c0b92bfaff77d22603177c9d17006a50e56e8d08
Marc Godwin <marcgodwinsigningdongmo@gmail.com>
2026-06-15 02:00:49 +0200

    new link sitmaps

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+// src/app/sitemap.js
+
+export default function sitemap() {
+  return [
+    {
+      url: 'https://landing-page-ten-cyan-35.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+  ];
+}
